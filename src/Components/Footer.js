59a41a98f0ba0a8b47b6517d73559f499cca9e01
Jongho Kim <april9288@gmail.com>
2018-10-03 13:15:02 -0700

    updated footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,8 +5,21 @@ const Footer = () => {
 	return (
 		<section className = "footer_section">
  		  <div className = "footer_section_left">	
- 		  	  <p>Zero To Mastery Community </p>
-			  <img style = {{borderRadius:10}} src={ZTM_Logo} alt="Zero To Mastery Community" height={80} />
+			<img style = {{borderRadius:10}} src={ZTM_Logo} alt="Zero To Mastery Community" height={100} />
+ 		  </div>
+ 		  <div>
+ 		  	<p style={{fontSize : "1.2rem", margin: 0}}>Zero To Mastery Community </p>
+
+		 		  	<div style={{margin: "1.2rem 0"}}>
+		 		  		<a className = "footer_links" href="https://zerotomastery.io/">Community Website</a>
+		 		  	</div>
+		 		  	<div style={{margin: "1.2rem 0"}}>
+		 		  		<a className = "footer_links" href="https://github.com/zero-to-mastery">Community Github</a>
+		 		  	</div>
+		 		  	<div style={{margin: "1.2rem 0"}}>
+		 		  		<a className = "footer_links" href="https://zero-to-mastery.github.io/ZtM-Job-Board/">Community Job Board</a>
+		 		  	</div>
+
  		  </div>
 		</section>
 		);
